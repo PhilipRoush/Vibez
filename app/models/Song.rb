5d274f4e82e1b9f12bce798f30a4f5942313e6b1
reedanna <reedanna@mail.com>
2020-08-17 17:22:16 -0500

    added ability to create new artist and genre to song#new

--- app/models/Song.rb
+++ app/models/Song.rb
@@ -12,4 +12,14 @@ class Song < ApplicationRecord
    validates_date :release_date, :on_or_before => lambda { Date.current }
    validates :title, uniqueness: {scope: :artist_id}
 
+   def artist_attributes=(artist)
+      self.artist = Artist.find_or_create_by(name: artist[:name])
+      self.artist.update(artist)
+   end
+
+   def genre_attributes=(genre)
+      self.genre = Genre.find_or_create_by(name: genre[:name])
+      self.genre.update(genre)
+   end
+
 end
